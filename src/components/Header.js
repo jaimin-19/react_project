a9e7703c691ba8cb3b65b9295e6cc54aef1b2458
jaimin patel <jaiminpatel1516453@gmail.com>
2025-09-20 11:39:23 +0530

    xx

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     
   <nav class="navbar navbar-expand-lg">
     <div class="container">
-      <a class="navbar-brand text-white fw-bold" href="#">
+      <a class="navbar-brand text-white fw-bold" href="/">
         <img src={logo} alt="Logo" class="me-2"/>
       </a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
@@ -14,23 +14,23 @@ function Header() {
       </button>
       <div class="collapse navbar-collapse nav-dark" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link" href="#">HOME</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">ABOUT US</a></li>
+          <li class="nav-item"><a class="nav-link" href="/">HOME</a></li>
+          <li class="nav-item"><a class="nav-link" href="/">ABOUT US</a></li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">PROJECTS</a>
+            <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown">PROJECTS</a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Project </a></li>
+              <li><a class="dropdown-item" href="/">Project </a></li>
             </ul>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">PRODUCTS</a>
+            <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown">PRODUCTS</a>
             <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Product 1</a></li>
-              <li><a class="dropdown-item" href="#">Product 2</a></li>
+              <li><a class="dropdown-item" href="/">Product 1</a></li>
+              <li><a class="dropdown-item" href="/">Product 2</a></li>
             </ul>
           </li>
-          <li class="nav-item"><a class="nav-link" href="#">MEDIA COVERAGE</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">CONTACT US</a></li>
+          <li class="nav-item"><a class="nav-link" href="/">MEDIA COVERAGE</a></li>
+          <li class="nav-item"><a class="nav-link" href="/">CONTACT US</a></li>
         </ul>
       </div>
     </div>
