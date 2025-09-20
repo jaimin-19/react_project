d3aa8c433b95fb8da9c4d2f8bd0540e7f6e79eb5
jaimin patel <jaiminpatel1516453@gmail.com>
2025-09-20 10:57:21 +0530

    a

--- src/components/Footor.js
+++ src/components/Footor.js
@@ -68,17 +68,17 @@ function Footor() {
           <img src={img} style={{width: "80%"}} class="mb-3" alt='I'/>
         <p>Yahska Polymers Pvt Ltd is a leading construction chemicals manufacturer based in Ahmedabad, proudly serving the Indian construction industry with innovative and reliable solutions for over two decades.</p>
         <div class="footer-social">
-          <a href="#"><i class="fab fa-twitter"></i></a>
-          <a href="#"><i class="fab fa-facebook-f"></i></a>
-          <a href="#"><i class="fab fa-linkedin-in"></i></a>
-          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="javascript:void(0)"><i class="fab fa-twitter"></i></a>
+          <a href="javascript:void(0)"><i class="fab fa-facebook-f"></i></a>
+          <a href="javascript:void(0)"><i class="fab fa-linkedin-in"></i></a>
+          <a href="javascript:void(0)"><i class="fab fa-instagram"></i></a>
         </div>
       </div>
 
       {/* <!-- Quick Links --> */}
       <div class="col-md-3">
         <h5 class="footer-title">Quick Links</h5>
-        <a href="#">Home</a>
+        <a href="javascript:void(0)">Home</a>
         <a href="#">About Us</a>
         <a href="#">Projects</a>
         <a href="#">Products</a>
